23b7ac93c5d535c97a30a6a6664ff32d6b705f76
sloth <sloth@kookmin.ac.kr>
2019-03-20 16:25:51 +0900

    todos API에 변경된 DB와 로그인 인증 연동 - 바뀐 DB 쿼리 적용 - 로그인 되었는지 확인하는 부분 추가 - test 추가 및 변경

--- routes/todos.js
+++ routes/todos.js
@@ -2,16 +2,21 @@ const express = require('express');
 const db = require('../mysql_db');
 const router = express.Router();
 
-const SELECT_ALL = 'SELECT * FROM todo';
-const SELECT_ONE = 'SELECT * FROM todo WHERE todo_id=?';
-const INSERT_ONE = 'INSERT INTO todo (text, is_complete) VALUES (?, ?)';
-const UPDATE_ONE = 'UPDATE todo SET text=?, is_complete=? WHERE todo_id = ?';
-const DELETE_ONE = 'DELETE FROM todo WHERE todo_id = ?';
+const SELECT_ALL = 'SELECT * FROM todo WHERE user_id = ?';
+const SELECT_ONE = 'SELECT * FROM todo WHERE user_id = ? AND todo_id = ?';
+const INSERT_ONE = 'INSERT INTO todo (user_id, text, is_complete) VALUES (?, ?, ?)';
+const UPDATE_ONE = 'UPDATE todo SET text=?, is_complete=? WHERE user_id=? AND todo_id=?';
+const DELETE_ONE = 'DELETE FROM todo WHERE user_id = ? AND todo_id = ?';
 
 const Todo = {
   // 모든 todo list 반환
   getAllTodos: function(req, res) {
-    db.query(SELECT_ALL, function(err, rows) {
+    if (!req.session.uid) {
+      res.sendStatus(401);
+      return;
+    }
+
+    db.query(SELECT_ALL, [req.session.uid], function(err, rows) {
       if (err) throw err;
       res.send(rows);
     });
@@ -19,6 +24,11 @@ const Todo = {
 
   // todo 만들기
   createTodo: function(req, res) {
+    if (!req.session.uid) {
+      res.sendStatus(401);
+      return;
+    }
+
     const todo_text = req.body.text;
     const is_complete = 0;
 
@@ -28,7 +38,7 @@ const Todo = {
       return ;
     }
 
-    db.query(INSERT_ONE, [todo_text, is_complete],
+    db.query(INSERT_ONE, [req.session.uid, todo_text, is_complete],
       function (err, results) {
         if (err) throw err;
         
@@ -46,8 +56,13 @@ const Todo = {
       res.sendStatus(400);
       return ;
     }
+
+    if (!req.session.uid) {
+      res.sendStatus(401);
+      return;
+    }
     
-    db.query(SELECT_ONE, [id], function (err, rows) {
+    db.query(SELECT_ONE, [req.session.uid, id], function (err, rows) {
       if (err) throw err;
       res.send(rows);
     });
@@ -62,12 +77,22 @@ const Todo = {
       res.sendStatus(400);
       return ;
     }
+
+    if (!req.session.uid) {
+      res.sendStatus(401);
+      return;
+    }
  
     let todo_text = req.body.text;
     let is_complete = req.body.is_complete;
 
-    db.query(SELECT_ONE, [id], function (err, rows) {
+    db.query(SELECT_ONE, [req.session.uid, id], function (err, rows) {
       if (err) throw err;
+
+      if (rows.length == 0) {
+        res.sendStatus(400);
+        return;
+      }
       
       if (todo_text == undefined)
         todo_text = rows[0].text;
@@ -76,15 +101,14 @@ const Todo = {
         is_complete = rows[0].is_complete;
 
       is_complete = is_complete == true;
-      db.query(UPDATE_ONE, [todo_text, is_complete, id],
-        function (err, results) {
+      db.query(UPDATE_ONE, [todo_text, is_complete, req.session.uid, id], function (err, results) {
           if (err) throw err;
 
           res.sendStatus(200);
         }
       );
     });
-},
+  },
 
   // id에 해당하는 Todo 지우기
   deleteTodo: function(req, res) {
@@ -95,8 +119,13 @@ const Todo = {
       res.sendStatus(400);
       return ;
     }
+
+    if (!req.session.uid) {
+      res.sendStatus(401);
+      return;
+    }
  
-    db.query(DELETE_ONE, [id], function (err, results) {
+    db.query(DELETE_ONE, [req.session.uid, id], function (err, results) {
       if (err) throw err;
 
       res.sendStatus(200);
--- tests/routes/todos.test.js
+++ tests/routes/todos.test.js
@@ -2,8 +2,14 @@ const expect = require('chai').expect;
 const request = require('supertest');
 const app = require('../../app');
 const db = require('../../mysql_db');
+const bcrypt = require('bcrypt');
 
-const test_data = [
+const test_user = {
+  username: 'todo_test1',
+  password: '1234'
+}
+
+const origin_data = [
   {
     text: 'test1',
     is_complete: 1
@@ -18,11 +24,68 @@ const test_data = [
   }
 ]
 
+let test_data;
+let sessionId;
+
+app.get('/mock/login', function(req, res) {
+  req.session.uid = test_user.user_id;
+  res.sendStatus(200);
+});
+
+app.get('/mock/logout', function(req, res) {
+  if (req.session.uid) {
+    req.session.destroy(function(err) {
+      res.sendStatus(200);
+    });
+  }
+  else
+    res.sendStatus(404);
+});
+
+const login = (done) => {
+  request(app)
+    .get('/mock/login')
+    .expect(200)
+    .end(function(err, res) {
+      sessionId = res.headers['set-cookie'][0].split(';')[0];
+      done();
+    });
+};
+const logout = (done) => {
+  request(app)
+    .get('/mock/logout')
+    .set('cookie', sessionId)
+    .expect(200, done);
+};
+
 describe('Todos Route', function() {
-  // 각 테스트의 테스트 데이터 초기화
-  before(function() {
+  // 테스트용 user 데이터 추가
+  before((done) => {
+    db.query("SELECT * FROM user WHERE username=?", [test_user.username], function(err, rows) {
+      if (err) expect.fail();
+      if (rows.length == 0) {
+        const salt = bcrypt.genSaltSync(10);
+        const hash = bcrypt.hashSync(test_user.password,salt);
+
+        db.query("INSERT INTO user (username, password) VALUES (?, ?)", [test_user.username, hash], function (err, result) {
+          if (err) expect.fail(err);
+          test_user.user_id = result.insertId;
+          login(done);
+        });
+      } else {
+        test_user.user_id = rows[0].user_id;
+        login(done);
+      }
+    });
+  });
+
+  // 매번 describe 마다 database의 todo 데이터 초기화
+  beforeEach((done) => {
+    // object array 복사
+    test_data = origin_data.map(todo => JSON.parse(JSON.stringify(todo)));
+
     db.query("DELETE FROM todo", function(err, result) {
-      db.query("INSERT INTO todo (text, is_complete) VALUES ?", [test_data.map(todo => Object.values(todo))], function(err, result) {
+      db.query("INSERT INTO todo (user_id, text, is_complete) VALUES ?", [origin_data.map(todo => [test_user.user_id, todo.text, todo.is_complete])], function(err, result) {
         if (err)
           return done(err);
         let insertId = result.insertId;
@@ -30,13 +93,21 @@ describe('Todos Route', function() {
           data.todo_id = insertId;
           insertId += 1;
         });
+
+        done();
       });
     });
   });
+
+  after((done) => {
+    logout(done);
+  });
+
   describe('GET /, getAll', function() {
     it('전체 리스트가 리턴되어야 함.', function(done) {
       request(app)
         .get('/todos')
+        .set('cookie', sessionId)
         .expect(200)
         .end((err, res) => {
           if (err) {
@@ -51,11 +122,18 @@ describe('Todos Route', function() {
           done();
         });
     });
+    it('로그인 되지 않음. 401 에러 반환.', function(done) {
+      request(app)
+        .get('/todos')
+        .expect(401, done);
+    });
   });
+
   describe('GET /:id, getOne', function() {
     it('빈 데이터 반환.', function(done) {
       request(app)
         .get(`/todos/${test_data[0].todo_id-1}`)
+        .set('cookie', sessionId)
         .expect(200)
         .end((err, res) => {
           if (err) return done(err);
@@ -67,6 +145,7 @@ describe('Todos Route', function() {
     it('하나의 데이터 반환.', function(done) {
       request(app)
         .get(`/todos/${test_data[0].todo_id}`)
+        .set('cookie', sessionId)
         .expect(200)
         .end((err, res) => {
           if (err) return done(err);
@@ -76,66 +155,103 @@ describe('Todos Route', function() {
           done();
         });
     });
-    it('잘못된 URL 입력. 에러 반환.', function(done) {
+    it('로그인 되지 않음. 401 에러 반환.', function(done) {
+      request(app)
+        .get(`/todos/${test_data[0].todo_id}`)
+        .expect(401, done);
+    });
+    it('잘못된 URL id 입력. 400 에러 반환.', function(done) {
       request(app)
         .get('/todos/hello')
+        .set('cookie', sessionId)
         .expect(400, done);
     });
   });
+  
   describe('POST /, createOne', function() {
     it('추가된 데이터의 ID 반환', function(done) {
       const text = 123;
       request(app)
         .post('/todos')
+        .set('cookie', sessionId)
         .send({text: text})
         .expect(200)
         .end((err, res) => {
-          let newTodo = {};
-          newTodo.text = text;
-          newTodo.is_complete = 0;
-          newTodo.todo_id = res.body.insertId;
-          test_data.push(newTodo);
+          expect(typeof res.body.insertId).to.equal('number');
           done();
         });
     });
-    it('데이터 없는 입력. 에러 반환', function(done) {
+    it('데이터 없는 입력. 400 에러 반환', function(done) {
       request(app)
         .post('/todos')
+        .set('cookie', sessionId)
         .send({})
         .expect(400, done);
     });
+    it('로그인 되지 않음. 401 에러 반환.', function(done) {
+      request(app)
+        .post('/todos')
+        .send({text: 'dummy'})
+        .expect(401, done);
+    });
   });
+
   describe('PUT /:id, editOne', function() {
     it('빈 데이터 입력. OK 반환', function(done) {
       request(app)
         .put(`/todos/${test_data[0].todo_id}`)
+        .set('cookie', sessionId)
         .send({})
         .expect(200, done);
     });
     it('데이터 변경. status 200 반환', function(done) {
-      const newText = 'test1';
+      const newText = 'new_test1';
       request(app)
         .put(`/todos/${test_data[0].todo_id}`)
+        .set('cookie', sessionId)
         .send({text: newText})
         .expect(200, done);
     });
-    it('잘못된 url id 입력. 에러 반환', function(done) {
+    it('데이터 존재하지 않음. 400 에러 반환', function(done) {
+      const newText = 'new_test1';
+      request(app)
+        .put(`/todos/${test_data[0].todo_id - 1}`)
+        .set('cookie', sessionId)
+        .send({text: newText})
+        .expect(400, done);
+    });
+    it('잘못된 url id 입력. 400 에러 반환', function(done) {
       request(app)
         .put('/todos/hello')
+        .set('cookie', sessionId)
         .send({text: 'dummy'})
         .expect(400, done);
     });
+    it('로그인 되지 않음. 401 에러 반환.', function(done) {
+      request(app)
+        .put(`/todos/${test_data[0].todo_id}`)
+        .send({})
+        .expect(401, done);
+    });
   });
+
   describe('DELETE /:id, deleteOne', function() {
     it('데이터 삭제. status 200 반환', function(done) {
       request(app)
         .delete(`/todos/${test_data[0].todo_id}`)
+        .set('cookie', sessionId)
         .expect(200, done);
     });
-    it('잘못된 url id 입력. 에러 반환', function(done) {
+    it('잘못된 url id 입력. 400 에러 반환', function(done) {
       request(app)
         .delete('/todos/hello')
+        .set('cookie', sessionId)
         .expect(400, done);
+    });
+    it('로그인 되지 않음. 401 에러 반환.', function(done) {
+      request(app)
+        .delete(`/todos/${test_data[0].todo_id}`)
+        .expect(401, done);
     });
   });
 });
